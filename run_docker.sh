73b0ff30fbe4792011f7db79f14eeba51fb5fcbb
carlosb <carlosb@cvc.uab.es>
2017-06-14 17:26:21 +0200

    Added flag in script run_docker in order to permit use old keras versions

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 PATH_DATASET="/home/carlosb/datasets"
 PATH_SOURCE_CODE="$PWD/mcv-m5"
+KERAS_OLD=""
 
-sudo docker run -it -v="$PATH_DATASET:/srv/data" -v="$PATH_SOURCE_CODE:/srv/src/mcv-m5" --rm gw000/keras-full  ipython2
+while getopts "k" OPTION; do
+	case $OPTION in
+		k)
+			KERAS_OLD=":1.2.0"
+		;;	
+		*)
+			echo "./run_docker.sh [-k]"
+			exit 1
+		;;
+	esac
+
+done
+
+
+sudo docker run -it -v="$PATH_DATASET:/srv/data" -v="$PATH_SOURCE_CODE:/srv/src/mcv-m5" --rm gw000/keras-full$KERAS_OLD  ipython2
